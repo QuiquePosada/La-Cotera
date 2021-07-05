7278f9ebff2125568a67bcc32f43b3e65952b1ce
QuiquePosada <33169548+QuiquePosada@users.noreply.github.com>
2021-07-05 12:51:07 -0500

    Delete src/config directory

--- src/config/prod.js
+++
@@ -1,8 +0,0 @@
-// used as a good practice/reminder for future projects (this one only needs one set of keys)
-module.exports = {
-    SPACE_ID: process.env.SPACE_ID,
-    CONTENTFUL_DELIVERY_API: process.env.CONTENTFUL_DELIVERY_API,
-    CONTENTFUL_PREVIEW: process.env.CONTENTFUL_PREVIEW,
-    CONTENTFUL_PERSONAL_ACCESS_TOKEN: process.env.CONTENTFUL_PERSONAL_ACCESS_TOKEN,
-    MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
-} 
